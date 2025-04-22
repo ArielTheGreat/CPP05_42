61f25cfea1455bdf439afbc911eaf5b72c8ef857
frocha <frocha@student.42heilbronn.de>
2025-04-22 18:40:48 +0200

    added function to get attributes

--- ex01/Form.cpp
+++ ex01/Form.cpp
@@ -1,2 +1,21 @@
 #include "Form.hpp"
 
+const std::string Form::getName()
+{
+    return (name);
+}
+
+bool Form::getSigned()
+{
+    return (isSigned);
+}
+
+const int Form::getRequiredGradeSign()
+{
+    return (requiredGradeToSign);
+}
+
+const int Form::getRequiredGradeExecute()
+{
+    return (requiredGradeToExecute);
+}
